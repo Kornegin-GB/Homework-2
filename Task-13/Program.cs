1b2e2d667bd5087dfb2a0931557ce4c196f67f96
Sergey Filippenkov <fsvmxd@yandex.ru>
2022-06-14 00:45:40 +0300

    Решение задачи 13

--- Task-13/Program.cs
+++ Task-13/Program.cs
@@ -3,3 +3,14 @@
 // 645 -> 6
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите число: ");
+string? number = ReadLine();
+
+if (number?.Length < 3) WriteLine("Третьей цифры нет");
+else WriteLine($"Третья цифра числа {number} является {number?[2]}");
