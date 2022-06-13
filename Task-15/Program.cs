94263e7af0dbe3b3a6c77aedb60c4f15886c0204
Sergey Filippenkov <fsvmxd@yandex.ru>
2022-06-14 00:57:45 +0300

    Решение задачи 15

--- Task-15/Program.cs
+++ Task-15/Program.cs
@@ -3,3 +3,35 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите порядковый номер дня недели: ");
+int number = Convert.ToInt32(ReadLine());
+
+switch (number)
+{
+   case 1:
+   case 2:
+   case 3:
+   case 4:
+   case 5:
+      {
+         WriteLine("Рабочий день");
+         break;
+      }
+   case 6:
+   case 7:
+      {
+         WriteLine("Выходной день");
+         break;
+      }
+   default:
+      {
+         WriteLine("Такого дня недели нет");
+         break;
+      }
+}
